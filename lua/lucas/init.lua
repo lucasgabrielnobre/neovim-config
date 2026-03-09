cb33589a2e277096970caef889694797dd37e557
lucasgabrielnobre <lucasgabrielprofs@gmail.com>
2026-03-09 20:46:54 -0300

    Terminal, remaps, build

--- lua/lucas/init.lua
+++ lua/lucas/init.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.cmd("lcd %:p:h")
+  end
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.cmd("startinsert")
+  end,
+})
 require("lucas.lazy")
 require("lucas.remap")
 require("lucas.code-style")
--- lua/lucas/remap.lua
+++ lua/lucas/remap.lua
@@ -12,7 +12,15 @@ end)
 vim.keymap.set('n', '<leader>n', function()
   vim.o.number = not vim.o.number
 end)
-
 vim.keymap.set('n', '<C-n>', function()
   vim.wo.relativenumber = not vim.wo.relativenumber
+end)
+
+vim.keymap.set('n', '<C-a>', ':%y+<CR>', { noremap = true, silent = true, desc = 'Copy Entire File' })
+
+vim.keymap.set("n", "<F7>", ":!g++ -std=c++17 -Wall -O2 % -o %:r<CR>", { silent = true })
+vim.keymap.set("n", "<F2>", ":vs | terminal ./%:r<CR>", { silent = true })
+vim.keymap.set("n", "<F5>", function()
+  vim.cmd("w")
+  vim.cmd("split | terminal g++ -std=c++17 -Wall -O2 % -o %:r && ./%:r")
 end)
